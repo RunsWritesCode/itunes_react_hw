33e00e60e7b76425ab09a038556d356733f9350f
RunsWritesCode <sophieamullins@gmail.com>
2018-02-20 20:04:51 +0000

    used different image index, added retro iPod styling

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,18 +3,30 @@ import React from 'react';
 const Song = (props) => {
   if (!props.title) return null;
   return (
-    <div>
-      <p>
-        {props.chartPosition}
+    <div id="song-item">
+    <div id="row">
+      <div id="column">
+      <p id="number">
+        Number {props.chartPosition}
       </p>
+    </div>
+    <div id="column">
       <p id="title">
         {props.title}
       </p>
+        </div>
+    </div>
+  <div id="column">
       <p>
         {props.artist}
       </p>
+    </div>
+    <div id="row">
       <img src={props.image}/>
+    </div>
+      <div id="preview">
       <a href={props.preview}>Preview</a>
+    </div>
     </div>
   )
 }
